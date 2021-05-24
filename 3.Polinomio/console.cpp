22541f37833caddbc0cdc1b4c7dbe07832b7cfbc
Willy Vargas <willy4k@gmail.com>
2021-05-23 20:02:44 -0400

    Polinomio 2da parte

--- 3.Polinomio/console.cpp
+++ 3.Polinomio/console.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "UPoliL.h"
+#include "UPoliV.h"
+#include "UPoliP.h"
 
 using namespace std;
 
 int main() {
-	PoliL* p = new PoliL();
+	PoliP* p = new PoliP();
 	int v[] = {-1, 1, 3, 5, 4, 3};
 	cout << p->to_str() << endl;
 	for (int i = 0; i < 6; i += 2) {
@@ -15,7 +17,17 @@ int main() {
 	cout << "Coef de 1: " << p->coeficiente(1) << endl;
 	p->asignar_coeficiente(0, 5);
 	cout << p->to_str() << endl;
-	cout << "Exponente nro 2: " << p->exponente(2) << endl;;
+	cout << "Exponente nro 2: " << p->exponente(2) << endl;
+	/* int c = 1043;
+	 bool b = false;
+	 NodoPo* aux = new NodoPo();
+	 cout << NULL << endl;
+	 if (aux) {
+	 cout << "True" << endl;
+	 }
+	 else {
+	 cout << "Falso" << endl;
+	 } */
 	system("pause");
 	return 0;
 }
